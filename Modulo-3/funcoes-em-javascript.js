5441cbad48e114095ce172a966dad54aa07ead76
FehSchiavon <felipschiavon@gmail.com>
2020-10-24 14:30:44 -0300

    learn + function

---
+++ Modulo-3/funcoes-em-javascript.js
@@ -0,0 +1,11 @@
+function nomeDaFuncao (parametro1) {
+    // bloco de codigo
+}
+
+function queDiaEHoje() {
+    const data = new Date()
+    console.log(`Hoje é dia: ${data.getDate()}`)
+}
+
+queDiaEHoje()
+
