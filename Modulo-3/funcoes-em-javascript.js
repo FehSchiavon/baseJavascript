4d2a5da31f1d1896493a2c3221d2eeb101a63765
FehSchiavon <felipschiavon@gmail.com>
2020-10-24 14:43:16 -0300

    function escopo / chamando functions

--- Modulo-3/funcoes-em-javascript.js
+++ Modulo-3/funcoes-em-javascript.js
@@ -6,12 +6,31 @@ function queDiaEHoje() {
     const data = new Date()
     console.log(`Hoje é dia: ${data.getDate()}`)
 }
-
 // queDiaEHoje()
 
 function soma(valor1, valor2) {
     console.log(`A soma de ${valor1} + ${valor2} é`, valor1 + valor2)
 }
-
 // soma(23, 21)
+
+
+// Funcoes retornando valores
+function soma(valor1, valor2) {
+    return valor1 + valor2
+}
+
+const idade = 20
+const tamanho = 10
+const resultado = soma(idade, tamanho)
+
+console.log('Resultado', resultado)
+
+
+// Escopo das function
+function multiplicar(valor1, valor2) {
+    const resultado = valor1 * valor2
+    return resultado
+}
+
+console.log(`O resultado da multiplicação é:`, multiplicar(10, 30))
 
